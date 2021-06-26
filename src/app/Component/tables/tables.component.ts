a81ef1857ed7b45db9a18e61a37e073a0436df65
ericcoker18 <ericcoker18@Yahoo.com>
2021-06-26 17:00:55 -0500

    Commit Seven

--- src/app/Component/tables/tables.component.ts
+++ src/app/Component/tables/tables.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTable, MatTableDataSource } from '@angular/material';
+import { ITodo } from 'src/app/interfaces/itodo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-tables',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
-  constructor() { }
+  displayedcolumns: string[] = ['title', 'description', 'status'];
+  datasource: MatTableDataSource<ITodo>;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  constructor(private todo: TodoService) { }
 
   ngOnInit() {
+    this.datasource = new MatTableDataSource(this.todo.getTodos())
+    this.datasource.sort = this.sort;
   }
 
 }
